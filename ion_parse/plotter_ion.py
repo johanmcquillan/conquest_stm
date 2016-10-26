064a8963f647367efc00875e9a6697968009ce6c
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2016-10-26 21:41:27 +0100

    Added functionality for DZ and TZ and l>0

--- ion_parse/plotter_ion.py
+++ ion_parse/plotter_ion.py
@@ -1,16 +1,19 @@
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import math
 
+# Folder and names
+ionfolder = 'ions/'
+ionfiles = ['H_SZ_6.5au',	'H_SZP_6.5au', 'H_DZDP_6.5_2.5au',
+			'C_SZ_6.5au',	'C_SZP_6.5au', 'C_DZDP_6.5_2.5au',
+			'Si_SZ_8bohr']
 
-ionfiles = ['H_SZ_6.5au', 'C_SZ_6.5au']
-
-x = {}
-y = {}
+Radials = {}
 
 for ion in ionfiles:
-	f = open(ion+'.ion', 'r')
+	f = open(ionfolder+ion+'.ion', 'r')
 
-	foundPAO = False
+	Radials[ion] = {}
 
 	line = f.readline()
 
@@ -20,29 +23,46 @@ for ion in ionfiles:
 	for i in range(0, 9):
 		line = f.readline()
 
-	metadata = f.readline().split()
-	l = metadata[1]
-	n = metadata[2]
-	z = metadata[3]
+	line = f.readline()
 
-	metadata = f.readline().split()
-	pts = int(metadata[0])
+	while line.split()[0] != '#':
+		print ion, line
+		metadata = line.split()
+		l = metadata[0]
+		n = metadata[1]
+		z = metadata[2]
 
-	x[ion] = []
-	y[ion] = []
-
-	for i in range(0, pts):
+		orbitaldata = z+n+l
+		print orbitaldata
 		line = f.readline()
-		a, b = line.split()
-		x[ion].append(float(a))
-		y[ion].append(float(b))
+		metadata = line.split()
+		pts = int(metadata[0])
+		cutoff = float(metadata[2])
+
+		Radials[ion][orbitaldata] = [[], [], cutoff]
+
+		for i in range(0, pts):
+			line = f.readline()
+			a, b = line.split()
+			Radials[ion][orbitaldata][0].append(float(a))
+			Radials[ion][orbitaldata][1].append(float(b))
+
+		line = f.readline()
 
 with PdfPages('Rnl.pdf') as pdf:
 	for ion in ionfiles:
-			plt.plot(x[ion], y[ion])
-			plt.title('Radial Part for '+ion+'.ion')
-			plt.xlabel('Radius, $r$ / $a_0$')
-			plt.ylabel('$R_{nl}(r)$')
-			plt.grid(b=True, which='both', color='0.65',linestyle='--')
-			pdf.savefig()
-			plt.close()
+		orbitals = Radials[ion].keys()
+		orbitals = sorted(orbitals, key=lambda x: x)
+		cutoffs = []
+		for o in orbitals:
+			cutoffs.append(Radials[ion][o][2])
+			label = '$\zeta ='+o[0]+'$, $n='+o[1]+'$, $l='+o[2]+'$'#, $m_l='+o[2]+'$'
+			plt.plot(Radials[ion][o][0], Radials[ion][o][1], label=label)
+		plt.xlim(0, math.ceil(max(cutoffs)))
+		plt.title('Radial Functions for '+ion+'.ion')
+		plt.xlabel('Radius, $r$ / $a_0$')
+		plt.ylabel('$R_{nl}(r)$')
+		plt.grid(b=True, which='both', color='0.65',linestyle='--')
+		plt.legend()
+		pdf.savefig()
+		plt.close()
