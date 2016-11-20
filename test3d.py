ee16ccd8d6c8eea720dd0ccddcee694f26f92a9c
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2016-11-20 17:27:40 +0000

    Tested methane 3d charge densities

--- test3d.py
+++ test3d.py
@@ -3,9 +3,9 @@ import packages.plot as plot
 from packages.parser import Parser
 from packages.cell import Cell
 
-molecule = 'C6H6_TZTP'
+molecule = 'CH4_DZDP'
 
-prsr = Parser('ions/', ['C_SZ_6.5au', 'C_SZP_6.5au', 'C_DZDP_6.5_2.5au', 'C_TZTP_6.5_4.5_2.5au', 'H_SZ_6.5au', 'H_SZP_6.5au', 'H_DZDP_6.5_2.5au', 'H_TZTP_6.5_4.5_2.5au'], 'conquest/', [molecule])
+prsr = Parser('ions/', ['Si_SZ_8bohr', 'C_SZ_6.5au', 'C_SZP_6.5au', 'C_DZDP_6.5_2.5au', 'C_TZTP_6.5_4.5_2.5au', 'H_SZ_6.5au', 'H_SZP_6.5au', 'H_DZDP_6.5_2.5au', 'H_TZTP_6.5_4.5_2.5au'], 'conquest/', [molecule])
 prsr.parseIons()
 prsr.parseConquestOutput()
 atoms = prsr.atoms
@@ -14,14 +14,14 @@ electrons = prsr.electrons
 del prsr
 
 
-C = Cell(molecule+'_161118A', fl[molecule], electrons[molecule], 20.0, 20.0, 15.0, gridSpacing=.5)
+C = Cell(molecule+'_161120A', fl[molecule], electrons[molecule], 15.0, 15.0, 15.0, gridSpacing=.5)
 for a in atoms[molecule]:
 	C.addAtom(atoms[molecule][a], a)
 
 #C.combineBands((0.05, 0.1), normalise=True, debug=True)
 
 for i in range(0, len(C.bands)):
-	plot.plotChargeDensity3D(C, i, fraction=0.05, cmap=True, save=True, show=False)
+	plot.plotChargeDensity3D(C, i, fraction=0.1, cmap=True, save=True, show=False)
 
 #pltr.plotBasis3D('C_SZP_6.5au', 1, 3, 2, 0, show=False)
 #plt.show()
