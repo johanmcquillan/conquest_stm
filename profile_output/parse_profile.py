1608b3ed56c215e8f3915f35b5c86549cc298d88
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2016-11-30 23:09:48 +0000

    Ran profiled runs for different interpolations

--- profile_output/parse_profile.py
+++ profile_output/parse_profile.py
@@ -1,11 +1,17 @@
 
-profile = open("profile_161130_160647.dat")
+import argparse
+
+parser = argparse.ArgumentParser(description='Get filename')
+parser.add_argument('filename', metavar='i', type=str, help='File to read')
+
+filename = vars(parser.parse_args())['filename']
+profile = open(filename)
 
 profile.next()
 profile.next()
 profile.next()
 profile.next()
-profile.next()
+headers = profile.next().split()
 
 calls = []
 
@@ -14,10 +20,10 @@ for line in profile:
 
 calls = sorted(calls, key=lambda x: float(x[3]), reverse=True)
 
+width = '12'
+rowformat = '%'+width+'s %'+width+'s %'+width+'s %'+width+'s %'+width+'s %'+width+'s'
+
+print rowformat % (headers[0], headers[1], headers[2], headers[3], headers[4], headers[5])
 for i in range(20):
-	toPrint = ""
-	for j in range(len(calls[i])):
-		toPrint += calls[i][j]
-		if j != len(calls[i]) - 1:
-			toPrint += "   "
+	toPrint = rowformat % (calls[i][0], calls[i][1], calls[i][2], calls[i][3], calls[i][4], calls[i][5])
 	print toPrint
