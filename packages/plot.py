9bc919bc5bd94049b33c52cebc887eb237bb2c2c
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2017-01-31 13:20:57 +0000

    Changed PdfPages save argument to string literal for current plot

--- packages/plot.py
+++ packages/plot.py
@@ -555,7 +555,7 @@ def plot_current_2d(
 
 	title = cell.name+' current at $z='+str(z)+'a_0$'
 
-	with PdfPages('figures2D/'+save_name+'.pdf') as pdf:
+	with PdfPages(r'figures2D/'+save_name+'.pdf') as pdf:
 		plt.imshow(current, interpolation='bilinear', origin='lower', cmap=cm.copper)
 		plt.colorbar()
 		plt.title(title)
