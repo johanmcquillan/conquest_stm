ee788617ba0aeaa8a198d76e857574198055554f
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2017-02-01 10:52:26 +0000

    Fixed partial_surface option

--- packages/plot.py
+++ packages/plot.py
@@ -527,7 +527,7 @@ def plot_ldos_3d(
 
 def plot_current_2d(
 		cell, z, V, T, tip_work_func, tip_energy, delta_s, interpolation='cubic',
-		printStatus=False, recalculate=False, show=True, debug=False):
+		printStatus=False, recalculate=False, show=True, partial_surface=False, debug=False):
 	"""Plots cross-section of charge density to pdf.
 
 	All lengths measured in bohr radii (a0).
@@ -548,7 +548,7 @@ def plot_current_2d(
 		debug (bool, opt.): If true, print extra information during runtime
 	"""
 
-	current = cell.get_current_scan(z, V, T, tip_work_func, tip_energy, delta_s, recalculate=recalculate, debug=debug)
+	current = cell.get_current_scan(z, V, T, tip_work_func, tip_energy, delta_s, recalculate=recalculate, debug=debug, partial_surface=partial_surface)
 
 	timeStamp = '_{:%Y-%m-%d-%H-%M-%S}'.format(dt.datetime.now())
 	save_name = cell.name + '_current_' + str(z) +'_' + str(V) + '_' + str(T) + timeStamp
