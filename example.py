87865ac41286eb2670a295213b0df768d94eb90c
JMcQuillanMBAir <johan.mcquillan.13@ucl.ac.uk>
2018-05-12 17:19:18 +0100

    Update shebang to specify python2.7

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 import conquest_stm.plot as plot
 from conquest_stm.parser import get_cell
--- test_sph.py
+++ test_sph.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.7
 
 import conquest_stm.plot as plot
 
